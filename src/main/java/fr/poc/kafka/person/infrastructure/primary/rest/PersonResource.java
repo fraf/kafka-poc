831b2ca8242018d2369660e4a86bbdb454430532
ffournel <ffournel.pro@gmail.com>
2026-06-17 17:01:28 +0200

    Ajout d'une route de generation de personne. Ajout de la librairie DataFaker

--- src/main/java/fr/poc/kafka/person/infrastructure/primary/rest/PersonResource.java
+++ src/main/java/fr/poc/kafka/person/infrastructure/primary/rest/PersonResource.java
@@ -1,5 +1,6 @@
 package fr.poc.kafka.person.infrastructure.primary.rest;
 
+import fr.poc.kafka.helper.PersonFaker;
 import fr.poc.kafka.openapi.model.PersonDto;
 import fr.poc.kafka.openapi.rest.PersonApi;
 import fr.poc.kafka.person.application.PersonApplicationService;
@@ -7,14 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PersonResource implements PersonApi {
 
     private final PersonApplicationService personApplicationService;
+    private final PersonFaker personFaker;
 
 
-    public PersonResource(PersonApplicationService personApplicationService) {
+    public PersonResource(PersonApplicationService personApplicationService, PersonFaker personFaker) {
         this.personApplicationService = personApplicationService;
+        this.personFaker = personFaker;
     }
 
     public ResponseEntity<PersonDto> getPerson(Long id) {
@@ -22,7 +27,18 @@ public class PersonResource implements PersonApi {
         return new ResponseEntity<>(personDto, personDto == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 
+    @Override
+    public ResponseEntity<List<PersonDto>> getPersons() {
+        //TODO
+        return null;
+    }
+
     public ResponseEntity<PersonDto> createOrUpdatePerson(PersonDto personneSk, Boolean sendNotification) {
         return new ResponseEntity<>(personApplicationService.createOrUpdatePersonDto(personneSk, sendNotification != null && sendNotification), HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<PersonDto> generatePerson() {
+        return new ResponseEntity<>(personFaker.generateDto(), HttpStatus.OK);
     }
 }
